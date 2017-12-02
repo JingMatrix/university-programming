abe0b6145bd40b2b770ffc93a9e1ec0347bda919
FMC Operator <jingmatrix@qq.com>
2017-12-02 19:33:18 +0800

    Rename
    Standardization File Directory

--- matlab/homework_design/Class1/HilbertMatrix.m
+++
@@ -1,3 +0,0 @@
-function A=HilbertMatrix(n)
-X = 1:n;
-A= 1./(bsxfun(@plus,X',X)-1);
--- matlab/homework_design/Class1/polyeq.m
+++
@@ -1,7 +0,0 @@
-function polyeq(f1,f2,f3,f4)
-B=transpose(1:4);
-A=repmat(B,1,4).^repmat([3,2,1,0],4,1);
-sl=A\[f1 f2 f3 f4]';
-x=linspace(1,10);
-y=polyval(sl,x);
-plot(x,y)
